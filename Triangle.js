fbbe958b59952d32d962c537224cc5f1e1686325
ANGELES CAMPOS ORTIZ <consu.imagen@hotmail.com>
2018-06-07 10:05:46 -0500

    Creando un Triangulo

---
+++ Triangle.js
@@ -0,0 +1,22 @@
+Class Forma {
+    constructor (id, A, B, C, ladoA, ladoB, lado C)
+	{
+	   this.id=id
+	//asigna el id lo que pasa
+	//al constructor a un atributo de la
+	// clase que se  llama id
+	   this.mover (id, A, B, C, ladoA, ladoB, lado C)
+	}
+     mover (id, A, B, C, ladoA, ladoB, lado C)
+	{
+	   this ladoA=ladoA
+	   this ladoB=ladoB
+	   this ladoC=ladoC
+	}
+}
+class Triangle extends Forma {
+	constructor (id, A, B, C, ladoA, ladoB, lado C)
+	   { super id,A, B, C)
+	   this lado=lado
+	}
+}
