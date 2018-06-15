089b013aef4c1b5bcd87b0ed8d18ac289f93e169
HeckGarcia <nosoc6@gmail.com>
2018-06-14 22:21:15 -0500

    Actualizacion

--- HectorJavierRiosGarcia/Tarea-moral.js
+++
@@ -1,9 +0,0 @@
-var mes = prompt ("Indique mes de nacimiento");
-var dia = prompt ("Indique dia de nacimiento");
-
-if (mes === "marzo" && dia >= 21 || mes === "abril" && dia <= 20){ 
-console.log("Eres Aries") 
-
-}else if (mes === "abril" && dia >= 21 || mes === "mayo" && dia <= 20) {
-console.log("Eres Tauro") 
-
